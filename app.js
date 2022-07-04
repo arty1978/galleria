206bb098775928b9d878dd5ac87a9cde61f5b423
arty1978 <artyomkin@gmail.com>
2022-07-05 00:10:00 +0300

    sort update

--- app.js
+++ app.js
@@ -1,78 +1,85 @@
-import { pictures } from './data/pictures.js';
-// console.log(pictures);
+import { pictures } from "./data/pictures.js";
 let pictures1 = [];
+
+document.getElementById("sort-field").addEventListener("change", function () {
+  // document.getElementById("sort-field").onchange = function () {
+  let sortArray = document.getElementById("sort-field");
+  // let str = sortArray.options[sortArray.selectedIndex].value;
+  // console.log("str value", str);
+  // if (sortArray.value === str) {
+  //   sortAtoZ(pictures);
+  // } else {
+  //   sortZtoA(pictures);
+  // }
+  sortArray.value === "asc" ? sortAtoZ(pictures) : sortZtoA(pictures);
+  //   sortArray.value === "desc" ? sortZtoA(pictures): sortAtoZ(pictures);
+  // console.log('sortArray.value',sortArray);
+});
+
 window.searchPic = function () {
-    const searchField = document.getElementById('search-field');
-    // console.log(searchField);
-    const searchTerm = searchField.value.trim(); // ' tab '.trim() => 'tab'
+  const searchField = document.getElementById("search-field");
+  const searchTerm = searchField.value.trim(); // ' tab '.trim() => 'tab'
 
-    const searchResult = pictures.filter(pic => {
-        return pic.author
-            .toLowerCase()
-            .includes(searchTerm.toLowerCase())
-    });
-    sortAtoZ(pictures);
-    sortZtoA(pictures);
-    console.log(searchResult);
-    displayGallery(searchResult);
-}
-// window.
+  const searchResult = pictures.filter((pic) => {
+    if (searchField.value === "author") {
+      return pic.author.toLowerCase().includes(searchTerm.toLowerCase());
+    } else {
+      return pic.title.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+  });
+  sortAtoZ(pictures);
+  sortZtoA(pictures);
+  console.log(searchResult);
+  displayGallery(searchResult);
+};
+
 function sortZtoA(pictures) {
-    const sort = document.getElementById('sort-field');
-    let sortDesc = [...pictures]
-    if (sort.value === 'desc') {
+  let sortDesc = [...pictures];
 
-        sortDesc.sort((a, b) => {
-            if (a.title < b.title) {
-                return 1;
-            }
-            if (a.title > b.title) {
-                return -1;
-            }
-            return 0;
-        });
-        console.log(22222, sortDesc);
-        pictures1 = sortDesc;
+  sortDesc.sort((a, b) => {
+    if (a.title < b.title) {
+      return 1;
     }
-    console.log('pictures1 Array', pictures1);
-    displayGallery(pictures1);
-};
+    if (a.title > b.title) {
+      return -1;
+    }
+    return 0;
+  });
 
-// sortZtoA(pictures);
+  console.log(22222, sortDesc);
+  pictures1 = sortDesc;
+  console.log("pictures1 Array", pictures1);
+  displayGallery(pictures1);
+}
 
-// window.
 function sortAtoZ(pictures) {
-    const sort = document.getElementById('sort-field');
-    let localArray = [...pictures];
-    if (sort.value === 'asc') {
+  let localArray = [...pictures];
 
-        localArray.sort((a, b) => {
-            if (a.title < b.title) {
-                return -1;
-            }
-            if (a.title > b.title) {
-                return 1;
-            }
-            return 0;
-        });
-        console.log(11111, localArray);
-        pictures1 = localArray;
+  localArray.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
     }
-    console.log('pictures1 Array', pictures1);
-    displayGallery(pictures1);
-};
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
+  console.log(11111, localArray);
+  pictures1 = localArray;
 
-// sortAtoZ(pictures);
+  console.log("pictures1 Array", pictures1);
+  Availibility(pictures1);
+  displayGallery(pictures1);
+}
 
 function displayGallery(pictures1) {
-    //pics structure 
-    const picsContainer = document.querySelector('.container');
-    let html = '';
+  //pics structure
+  const picsContainer = document.querySelector(".container");
+  let html = "";
 
-    //pics = pictures
-    pictures1.forEach(picElement => {
-        html +=
-            `<div class="card" style="width: 18rem;">
+  //pics = pictures
+  pictures1.forEach((picElement) => {
+    html += `<div class="card" style="width: 18rem;">
                                 <h3 name of the work of art class="card-title">Name of the art work: 
                         ${picElement.getTitle()}</h3>
                 <img src="${picElement.getPaint()}"
@@ -85,50 +92,24 @@ function displayGallery(pictures1) {
                     <a href="#" class="btn btn-primary">View Details</a>
                 </div>
             </div>`;
-    });
-    picsContainer.innerHTML = html;
-
+  });
+  picsContainer.innerHTML = html;
 }
-
-Availibility(pictures);
+displayGallery(pictures);
 
 function Availibility(picElement) {
-
-    for (let i = 0; i < picElement.length; i++) {
-
-        if (picElement[i].inStock) {
-
-            const inStock = document.querySelectorAll('.stock');
-            inStock[i].style.color = 'green';
-            inStock[i].style.fontSize = '1.5rem';
-
-        } else {
-
-            const inStock = document.querySelectorAll('.stock');
-            inStock[i].style.color = 'red';
-            inStock[i].style.fontWeight = '900';
-            inStock[i].style.fontSize = '1.5rem';
-        }
+  for (let i = 0; i < picElement.length; i++) {
+    if (picElement[i].inStock) {
+      console.log(picElement[i].inStock);
+      const inStock = document.querySelectorAll(".stock");
+      inStock[i].style.color = "green";
+      console.log(inStock[i].style.color);
+      inStock[i].style.fontSize = "1.5rem";
+    } else {
+      const inStock = document.querySelectorAll(".stock");
+      inStock[i].style.color = "red";
+      inStock[i].style.fontWeight = "900";
+      inStock[i].style.fontSize = "1.5rem";
     }
+  }
 }
-// using forEach Method
-// pictures.forEach((picElement, i) => {
-//     if (picElement.inStock) {
-//         const inStock = document.querySelectorAll('.stock');
-//         inStock[i].style.color = 'green';
-//     } else {
-//         const inStock = document.querySelectorAll('.stock');
-//         inStock[i].style.color = 'red';
-//         inStock[i].style.fontWeight = '900';
-//         inStock[i].style.fontSize = '1.5rem';
-//     }
-// });
-// }
-
-
-
-
-
-
-
-
